93dc61e84ad61796401b11ebb99a0bc4150c5446
Sam <greydanus@users.noreply.github.com>
2020-08-28 16:23:23 -0700

    Don't deploy to port 5000

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def index():
     # return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('PORT',5000))
+    app.run(debug=True)  # port=os.getenv('PORT',5000)
